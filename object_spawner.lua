e05dfa1c5f125a96d2808ca152527376de0307e9
Vijay K <76635804+vkethana@users.noreply.github.com>
2023-11-07 16:16:12 -0800

    Add files via upload

---
+++ object_spawner.lua
@@ -0,0 +1,150 @@
+-- Function to move the object
+local function moveObject(movingObject)
+	print("Type of object: " .. typeof(movingObject) .. "	ClassName: " ..  movingObject.className)
+	--print(movingObject.Anchored)
+	--movingObject.Anchored = true
+	if movingObject:IsA("Model") then
+		print("Object is a model.")
+		movingObject:TranslateBy(Vector3.new(10, 0, 0)) -- Move the Model 10 units upwards
+	else
+		print("Object is a mesh part.")
+		local destination = Vector3.new(0, 10, 0) -- Destination position (modify as needed)
+		local speed = 10 -- Movement speed (modify as needed)
+
+		local direction = (destination - movingObject.Position).unit
+		local distance = (destination - movingObject.Position).Magnitude
+
+		local timeToMove = distance / speed
+		local startTime = tick()
+
+		while tick() - startTime < timeToMove do
+			local newPosition = movingObject.Position + direction * speed * wait()
+			movingObject.Position = newPosition
+		end
+
+		movingObject.Position = destination
+	end
+end
+
+-- Function to get the asset name by ID
+local function getAssetNameById(assetId)
+	local success, assetInfo = pcall(function()
+		return game:GetService("MarketplaceService"):GetProductInfo(assetId)
+	end)
+
+	if success then
+		print(assetInfo)
+		return assetInfo["Name"] 
+	else
+		return "Unknown Asset"
+	end
+end
+
+local myDictionary = {
+	["dog"] = 257489726,
+	["car"] = 3946650318,
+	["table"] = 839550130,
+	["cat"] = 1718821907
+}
+
+local function getPlayerLocation()
+	return game.Players.JuicyBearHamm.Character.HumanoidRootPart.Position
+end
+
+local function spawnObject(assetId)
+	local success, modelOrPart = pcall(function()
+		return game:GetService("InsertService"):LoadAsset(assetId)
+	end)
+
+	if success then
+		if modelOrPart:IsA("Model") or modelOrPart:IsA("BasePart") then
+			local modelName = getAssetNameById(assetId)
+			local playerLocation = getPlayerLocation()
+			playerLocation = Vector3.new(playerLocation.X + 3, playerLocation.Y, playerLocation.Z)
+			modelOrPart.Parent = game.Workspace
+			local part = modelOrPart:FindFirstChild(modelName)
+			-- print("Object with name " .. modelName .. " spawned successfully!")
+			-- print("Info about and Position of the part is: ")
+			-- print(part)
+			
+			
+			-- print(playerLocation)
+			-- print(typeof(playerLocation))
+			--print(part.Position)
+			
+
+			-- local Destination = CFrame.new(playerLocation)
+			-- print(Destination)
+			-- modelName:SetPrimaryPartCFrame(Destination)
+		
+			-- print("Location reassigned successfully")
+			modelOrPart:MoveTo(playerLocation)
+			wait(5)
+			moveObject(part)
+		else
+			modelOrPart:Destroy()
+			print("Invalid object type.")
+		end
+	else
+		print("Failed to load object with Asset ID: " .. assetId)
+	end
+end
+
+-- Function to handle chat messages
+local function onPlayerChatted(player, message)
+	-- Check if the key exists in the dictionary
+	print("Player just said " .. message)
+	if myDictionary[message] ~= nil then
+		print("Key exists! Will generate: " .. myDictionary[message])
+		spawnObject(myDictionary[message])
+	else
+		print("Key does not exist.")
+	end
+end
+
+-- Connect the function to the Chatted event of each player
+game.Players.PlayerAdded:Connect(function(player)
+	player.Chatted:Connect(function(message)
+		onPlayerChatted(player, message)
+	end)
+end)
+
+--[[
+
+local HttpService = game:GetService("HttpService")
+
+local URL_ASTROS = "http://api.open-notify.org/astros.json"
+
+-- Make the request to our endpoint URL
+local response = HttpService:GetAsync(URL_ASTROS)
+
+-- Parse the JSON response
+local data = HttpService:JSONDecode(response)
+
+
+-- Information in the data table is dependent on the response JSON
+if data.message == "success" then
+	print("There are currently " .. data.number .. " astronauts in space:")
+	for i, person in pairs(data.people) do
+		print(i .. ": " .. person.name .. " is on " .. person.craft)
+	end
+end
+
+local HttpService = game:GetService("HttpService")
+
+local URL_ASTROS = "http://api.open-notify.org/astros.json"
+
+-- Make the request to our endpoint URL
+local response = HttpService:GetAsync(URL_ASTROS)
+
+-- Parse the JSON response
+local data = HttpService:JSONDecode(response)
+
+-- Information in the data table is dependent on the response JSON
+if data.message == "success" then
+	print("There are currently " .. data.number .. " astronauts in space:")
+	for i, person in pairs(data.people) do
+		print(i .. ": " .. person.name .. " is on " .. person.craft)
+	end
+end
+]]-- 
